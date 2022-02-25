e2910c7e24b51a09d8641cc6c27952bc040132d3
Andrew Lim <andrew0@live.com.sg>
2022-02-25 22:06:29 +0800

    working calculator

--- javascript.js
+++ javascript.js
@@ -1,48 +1,107 @@
-let values = [];
+const numberButtons = document.querySelectorAll('.number');
+const operationButtons = document.querySelectorAll('.operation');
+const equalsButton = document.querySelector('.equals');
+const deleteButton = document.querySelector('.delete');
+const allClearButton = document.querySelector('.clear');
+const previousDisplay = document.querySelector('.previous-operand');
+const currentDisplay = document.querySelector('.current-operand');
 
-const add = function(listOfitems){
-    return listOfitems[1]+listOfitems[0];
+
+const add = function(x,y){
+    return x+y;
+};
+const subtract = function(x,y){
+    return x-y;
+};
+const multiply = function(x,y){
+    return x*y;
+};
+const divide = function(x,y){
+    return x/y;
 };
 
-const subtract = function(listOfitems){
-    return listOfitems[1]-listOfitems[0];
-    };
-
-const multiply = function(listOfitems){
-    return listOfitems[1]*listOfitems[0];
-};
-
-const divide = function(listOfitems){
-    return listOfitems[1]/listOfitems[0];
-};
-
-const operate = function(listOfitems){
-console.log(listOfitems);
+const operate = function(operator,numberTwo,numberOne){
+    numberOne = parseInt(numberOne);
+    numberTwo = parseInt(numberTwo);
+if(operator === "+"){
+    return (numberOne + numberTwo);
+}
+else if(operator === "-"){
+    return (numberOne - numberTwo);
+}
+else if(operator === "*"){
+    return (numberOne * numberTwo);
+}
+else if(operator === "/"){
+    if(numberTwo === 0){
+        return null;
+    }
+    else{
+    return (numberOne / numberTwo);
+}
+}
 return 0;
 };
 
-const finalDisplay = function(e){
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach(button => button.addEventListener('click',function(e){
-        const button = document.querySelector(`buttons[id=${e.id}]`);
-        console.log(e.target.id);
-        const display = document.querySelector('.display');
-        if (e.target.className === "clears"){
-            display.textContent = '';
-            values = [];
-        }
-        else if(e.target.className === "equals" || e.target.className === "adds" 
-        || e.target.className === "minuses"|| e.target.className === "multiplies"
-        || e.target.className === "divides"){
-            display.textContent += e.target.id;
-            values.push(e.target.id);
-            operate(values);
-        }
-        else{
-            display.textContent += e.target.id;
-            values.push(e.target.id);
-        }
-    }))
-};
+let Array1 = [];
+let number = '';
+let currentOperator = [];
+let previousOperator = '';
+let previousArray = '';
+let previousNumber = '';
+let operatedNumber = '';
+let newPreviousArray = '';
+let reset = false;
 
-finalDisplay();
+numberButtons.forEach(button=>{
+    button.addEventListener('click',()=>{
+    Array1.push(button.id);
+    number = Array1.join("");
+    currentDisplay.textContent = number;
+})
+});
+
+operationButtons.forEach(button=>{
+    button.addEventListener('click',()=>{
+    if(number === null){
+        return;
+    }
+    currentOperator.push(button.id);
+    operatedNumber = operate(previousOperator,number,previousNumber);
+    if(previousOperator){
+    newPreviousArray = `${operatedNumber}${button.id}`;
+    currentDisplay.textContent = operatedNumber;
+    previousDisplay.textContent = newPreviousArray;
+    previousNumber = operatedNumber;
+    Array1 = [];
+    }
+    else if(currentOperator){
+    previousArray = `${number}${currentOperator[currentOperator.length-1]}`;
+    previousDisplay.textContent = previousArray;
+    previousNumber = number;
+    Array1 = [];
+    }
+    previousOperator = currentOperator[currentOperator.length-1];
+    number = null;
+})
+});
+
+
+
+
+allClearButton.addEventListener('click',()=>{
+currentDisplay.textContent = '';
+previousDisplay.textContent = '';
+Array1 = [];
+operatedNumber = 0;
+previousNumber='';
+currentOperator = '';
+});
+
+if(operatedNumber){
+    equalsButton.addEventListener('click',()=>{
+    operatedNumber = operate(button.id,number,previousNumber);
+    previousDisplay.textContent = `${newPreviousArray}${equalsButton.id}`;
+    currentDisplay.textContent = operatedNumber;
+    })
+}
